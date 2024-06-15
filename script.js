6e516a787cd0214f63ee6c32ebe6feb7b64552ba
Gisele215 <145718564+Gisele215@users.noreply.github.com>
2024-06-14 22:02:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function limparConsulta() {
+    document.getElementById('nomeConsulta').value = '';  // Limpa o campo de consulta
+    document.getElementById('listaFuncionarios').innerHTML = '';  // Limpa a lista de resultados
+}
+
+function limparConsultaRegistro() {
+    document.getElementById('dataConsulta').value = '';  // Limpa o campo de consulta
+    document.getElementById('listaPontos').innerHTML = '';  // Limpa a lista de resultados
+}
